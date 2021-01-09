64a903fc1a61a3b0d5a0e27c491ea2c4ab88eb8e
Matheus Abreu <matheus.abreu@olist.com>
2021-01-09 13:00:35 -0300

    Changed to class implementation, based on Django ViewSet.

--- index.js
+++ index.js
@@ -1,56 +1,83 @@
-function registerCRUD(expressInstance, routeName, sequelizeModel) {
-  var bodyParser = require("body-parser");
-  expressInstance.use(bodyParser.urlencoded({ extended: false }));
-  expressInstance.use(bodyParser.json());
+class coalCRUD {
+  constructor(app, routeName, sequelizeModel) {
+    this.app = app;
+    this.route = routeName;
+    this.model = sequelizeModel;
+    this._useBodyParser();
+  }
 
-  expressInstance.get("/" + routeName, async function (_, res) {
-    const result = await sequelizeModel.findAll();
-    res.status(200).send(result);
-  });
+  _useBodyParser() {
+    var bodyParser = require("body-parser");
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use(bodyParser.json());
+  }
 
-  expressInstance.get("/" + routeName + "/:id", async function (req, res) {
-    const id = req.params.id;
-    const result = await sequelizeModel.findByPk(id);
-    if (item === null) {
-      res.status(404).send();
-    } else {
-      res.status(200).send(result);
-    }
-  });
-
-  expressInstance.post("/" + routeName, async function (req, res) {
-    try {
-      const result = await sequelizeModel.create(req.body);
-      res.status(201).send(result);
-    } catch (_) {
-      res.status(500).send();
-    }
-  });
-
-  expressInstance.put("/" + routeName + "/:id", async function (req, res) {
-    const id = req.params.id;
-    const item = await sequelizeModel.findByPk(id);
-    if (item === null) {
-      res.status(404).send();
-    } else {
-      for (let [key, value] of Object.entries(req.body)) {
-        item[key] = value;
+  create() {
+    this.app.post("/" + this.route, async (req, res) => {
+      try {
+        const result = await this.model.create(req.body);
+        res.status(201).send(result);
+      } catch (_) {
+        res.status(500).send();
       }
-      const result = await item.save();
-      res.status(200).send(result);
-    }
-  });
+    });
+  }
 
-  expressInstance.delete("/" + routeName + "/:id", async function (req, res) {
-    const id = req.params.id;
-    const item = await sequelizeModel.findByPk(id);
-    if (item === null) {
-      res.status(404).send();
-    } else {
-      item.destroy();
-      res.status(204).send();
-    }
-  });
+  getOne() {
+    this.app.get("/" + this.route + "/:id", async (req, res) => {
+      const id = req.params.id;
+      const item = await this.model.findByPk(id);
+      if (item === null) {
+        res.status(404).send();
+      } else {
+        res.status(200).send(item);
+      }
+    });
+  }
+
+  getAll() {
+    this.app.get("/" + this.route, async (_, res) => {
+      const result = await this.model.findAll();
+      res.status(200).send(result);
+    });
+  }
+
+  update() {
+    this.app.put("/" + this.route + "/:id", async (req, res) => {
+      const id = req.params.id;
+      const item = await this.model.findByPk(id);
+      if (item === null) {
+        res.status(404).send();
+      } else {
+        for (let [key, value] of Object.entries(req.body)) {
+          item[key] = value;
+        }
+        const result = await item.save();
+        res.status(200).send(result);
+      }
+    });
+  }
+
+  delete() {
+    this.app.delete("/" + this.route + "/:id", async (req, res) => {
+      const id = req.params.id;
+      const item = await this.model.findByPk(id);
+      if (item === null) {
+        res.status(404).send();
+      } else {
+        item.destroy();
+        res.status(204).send();
+      }
+    });
+  }
+
+  registerCRUD() {
+    this.create();
+    this.update();
+    this.getOne();
+    this.getAll();
+    this.delete();
+  }
 }
 
-module.exports = registerCRUD
+module.exports = coalCRUD;
